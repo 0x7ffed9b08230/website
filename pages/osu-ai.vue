9e961f7278cc4545f9ee4c4d47f48c3c6e10ba88
Hayden Cox <hcox.dev@gmail.com>
2024-06-09 01:08:08 -0400

    feat(Videos): remove videos

--- pages/osu-ai.vue
+++ pages/osu-ai.vue
@@ -16,19 +16,26 @@ onMounted(() => {
             Linux, Android and iOS. Its gameplay is based on commercial titles including Osu! Tatakae! Ouendan,
             Elite Beat Agents, Taiko no Tatsujin, beatmania IIDX, O2Jam, and DJMax.</q>
     </p>
-    
+
 
     <p class="text-lg text-gray-600 dark:text-gray-400">
-        This is a video of the AI playing a song called <Links href="https://osu.ppy.sh/beatmapsets/783213#osu/1644684" text="UNDEAD CORPORATION - Embraced by the Flame" />.
-    </p>
+        This is a video of the AI playing a song called
+        <Links href="https://osu.ppy.sh/beatmapsets/783213#osu/1644684"
+            text="UNDEAD CORPORATION - Embraced by the Flame" />.
+        <br>
 
-    <video class="w-full" controls autoplay muted>
-        <source src="/static/videos/osu-ai/UNDEAD CORPORATION_Embraced by the Flame.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
+        <small><strong>NOTE: </strong>This video is uploaded to Google Drive, so it might not work on some devices. To
+            watch the video, <Links href="https://drive.google.com/file/d/1oW3ewf5LoY3Hz-GjXToDJzbmFBRTBF-d/view"
+                text="click here" />.</small>
+    </p>
+    <br>
+    <br>
+    <iframe src="https://drive.google.com/file/d/1oW3ewf5LoY3Hz-GjXToDJzbmFBRTBF-d/preview" width="1260" height="640"
+        allow="autoplay"></iframe>
 
     <p class="text-lg text-gray-600 dark:text-gray-400" style="margin-top: 1em;">
-        <strong>The AI is not open-source at the moment, as it needs a rewrite to be more user-friendly and developer-friendly.</strong>
+        <strong>The AI is not open-source at the moment, as it needs a rewrite to be more user-friendly and
+            developer-friendly.</strong>
     </p>
 
 
