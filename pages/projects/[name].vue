99d4eefad85d771276fa9c526d703022ffaf2571
Hayden Cox <hcox.dev@gmail.com>
2024-06-12 21:00:29 -0400

    feat(Nuxt): add ssr

--- pages/projects/[name].vue
+++ pages/projects/[name].vue
@@ -1,33 +1,29 @@
 <script setup lang="ts">
-const route = useRoute()
-const router = useRouter()
+import { ref, computed, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useHead, useSeoMeta } from '@vueuse/head';
 import { initFlowbite } from 'flowbite';
-import { GetArticleType } from '~/utils/articleType';
-import type { IDeveloper } from '../../interfaces/IDeveloper';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-dark.css';
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
+import type { IDeveloper } from '../../interfaces/IDeveloper';
 
+// Initialize route and router
+const route = useRoute();
+const router = useRouter();
+const name = ref(route.params.name);
 
-const name = ref(route.params.name)
-
-const {
-    data: infoData,
-    error: infoError,
-} = await useFetch<{ data: IDeveloper }>(
-    `/api/info`
-);
-
-
+// Fetch developer info
+const { data: infoData, error: infoError } = await useFetch<{ data: IDeveloper }>(`/api/info`);
 if (infoError.value) {
     console.error(infoError.value);
 }
 
-
+// Define computed properties and helper functions
 const info = infoData.value?.data;
 const _name = computed(() => name.value as string);
-const repo = info?.repos.find((repo) => repo.name === _name.value);
+const repo = info?.repos.find((repo: any) => repo.name === _name.value);
 
 const marked = new Marked(
     markedHighlight({
@@ -38,7 +34,6 @@ const marked = new Marked(
         }
     })
 );
-
 
 function formatDate(date: string) {
     const f = new Date(date).toLocaleDateString('en-US', {
@@ -61,55 +56,38 @@ function formatDate(date: string) {
     return obj;
 }
 
-
 if (!repo) {
-    router.push('/404')
+    router.push('/404');
 } else {
-
-
-
+    // Update head meta tags
     useHead({
         title: `${repo.name} - Projects`,
         meta: [
-            {
-                name: 'description',
-                content: repo.description
-            },
-            {
-                property: 'og:description',
-                content: repo.description
-            },
-            {
-                property: 'og:title',
-                content: `${repo.name} - Projects`
-            }
+            { name: 'description', content: repo.description },
+            { property: 'og:description', content: repo.description },
+            { property: 'og:title', content: `${repo.name} - Projects` }
         ],
-    })
+    });
 
     useSeoMeta({
         title: `${repo.name} - Projects`,
         description: repo.description,
-    })
+    });
 
     onMounted(async () => {
         initFlowbite();
+
         const content = document.querySelector('.blog-content');
         const readme_raw = ref<string>(repo?.readme || '');
-
 
         if (!content || !readme_raw.value) {
             return;
         }
 
         const readme = await marked.parse(readme_raw.value);
-
-        console.log(readme);
-
-
         content.innerHTML = readme;
 
         const images = content.querySelectorAll('img');
-
         images.forEach((image) => {
             const src = image.getAttribute('src');
             const alt = image.getAttribute('alt');
@@ -123,18 +101,13 @@ if (!repo) {
             }
 
             const figure = document.createElement('figure');
-
             const figcaption = document.createElement('figcaption');
-
             figcaption.textContent = alt;
 
             figure.appendChild(image.cloneNode(true));
-
             figure.appendChild(figcaption);
-
             image.replaceWith(figure);
         });
-
     });
 }
 
@@ -142,91 +115,72 @@ function makeLicense(c: string): string {
     return c.replace(/\n/g, '<br>');
 }
 
-
-
-
 </script>
+
 <template>
-    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-        <article
-            class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
             <header class="mb-4 lg:mb-6 not-format">
                 <address class="flex items-center mb-6 not-italic">
                     <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                         <img class="mr-4 w-16 h-16 rounded-full" :src="info.avatar_url" :alt="info.name" loading="lazy">
                         <div>
-                            <a :href="`https://github.com/${info.login}`" rel="author"
-                                class="text-xl font-bold text-gray-900 dark:text-white">{{ info?.name }} <span
-                                    class="text-base text-gray-500 dark:text-gray-400">(@{{ info?.login
-                                    }})</span></a>
-                            <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate
-                                    :datetime="repo.created_at" class="text-base text-gray-500 dark:text-gray-400">{{
-                                        formatDate(repo.created_at).date
-                                    }}</time> <span class="spacer"></span> <span
-                                    class="text-base text-gray-500 dark:text-gray-400"
-                                    data-tooltip-target="tooltip-updated">{{
-                                        formatDate(repo.updated_at).days }}</span></p>
-                            <div id="tooltip-updated" role="tooltip"
-                                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                This repository was last updated on <time pubdate :datetime="repo.updated_at">{{
-                                    formatDate(repo.updated_at).date
-                                    }}</time>
+                            <a :href="`https://github.com/${info.login}`" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">
+                                {{ info?.name }} <span class="text-base text-gray-500 dark:text-gray-400">(@{{ info?.login }})</span>
+                            </a>
+                            <p class="text-base text-gray-500 dark:text-gray-400">
+                                <time pubdate :datetime="repo.created_at" class="text-base text-gray-500 dark:text-gray-400">
+                                    {{ formatDate(repo.created_at).date }}
+                                </time>
+                                <span class="spacer"></span>
+                                <span class="text-base text-gray-500 dark:text-gray-400" data-tooltip-target="tooltip-updated">
+                                    {{ formatDate(repo.updated_at).days }}
+                                </span>
+                            </p>
+                            <div id="tooltip-updated" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                This repository was last updated on 
+                                <time pubdate :datetime="repo.updated_at">{{ formatDate(repo.updated_at).date }}</time>
                                 <div class="tooltip-arrow" data-popper-arrow></div>
                             </div>
                         </div>
                     </div>
                 </address>
-                <!-- Center it -->
                 <div id="actions">
                     <button id="view_license"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        type="button"
-                        data-modal-target="modal-license"
-                        data-modal-toggle="modal-license"
-                        >
+                        type="button" data-modal-target="modal-license" data-modal-toggle="modal-license">
                         View License
                         <i class="fa-solid fa-scale-balanced"></i>
                     </button>
-
-                    <a
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         type="button" :href="repo.html_url" target="_blank" rel="noopener noreferrer">
                         View on GitHub
                         <i class="fa-brands fa-github" style="color: white; font-size: 1.2rem;"></i>
-                </a>
-                    
+                    </a>
                 </div>
             </header>
-            <div class="blog-content dark:text-white mt-6 text-gray-900">
-
-            </div>
+            <div class="blog-content dark:text-white mt-6 text-gray-900"></div>
         </article>
     </div>
 
     <div id="modal-license" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    {{ repo?.license?.name || 'License' }} ({{ repo?.license?.spdx_id || 'N/A' }})
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-license">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        {{ repo?.license?.name || 'License' }} ({{ repo?.license?.spdx_id || 'N/A' }})
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-license">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <div class="p-4 md:p-5 space-y-4 dark:text-white" v-html="makeLicense(repo?.license?.content || 'NONE')"></div>
             </div>
-            <div class="p-4 md:p-5 space-y-4 dark:text-white" v-html="makeLicense(repo?.license?.content || 'NONE')">
-            </div>
-
         </div>
     </div>
-</div>
-    <br>
-    <br>
-    <br>
-    <br>
 </template>
 
 <style scoped>
@@ -239,7 +193,6 @@ function makeLicense(c: string): string {
         font-size: 0.875rem;
         color: #6b7280;
     }
-
 
     pre {
         margin-bottom: 1.5rem;
